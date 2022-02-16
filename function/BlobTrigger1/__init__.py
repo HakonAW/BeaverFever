53aefd95349d53f43a584b3fb000e70f689a387a
HakonAW <hakon.beckstrom@awacademyconsultant.fi>
2022-02-16 14:59:04 +0100

    working on function

--- function/BlobTrigger1/__init__.py
+++ function/BlobTrigger1/__init__.py
@@ -3,7 +3,8 @@ import logging
 import azure.functions as func
 
 
-def main(myblob: func.InputStream):
+def main(inputBlob: func.InputStream, ):
+
     logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+                 f"Name: {inputBlob.name}\n"
+                 f"Blob Size: {inputBlob.length} bytes")
