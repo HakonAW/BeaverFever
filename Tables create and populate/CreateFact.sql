4aea5a4a4f63725d5881868fae35b5ead48e17d9
HakonAW <hakon.beckstrom@awacademyconsultant.fi>
2022-02-21 10:40:11 +0100

    added schema to factSpecies and changed columns

--- Tables create and populate/CreateFact.sql
+++ Tables create and populate/CreateFact.sql
@@ -1,16 +1,17 @@
-Create table FactSpecies (
-[ObservationID]             int identity PRIMARY key,
- [Site_Code]                 varchar(10) ,
- [SpecieId]                  int  ,
- [YearMonth]                 int  ,
- [LOWERBOUND]                int  ,
- [UPPERBOUND]                int ,
- [Change_in_Conservation]    nvarchar(50)  ,
- [Change_in_Intro_candidate] nvarchar(50)  ,
- [Change_in_Global]          nvarchar(50) ,
- [CountingType]              char(10) ,
- [Dataquality]               char(1) ,
- [PopulationType]            char(1) ,
- [AboundanceCategory]        char(1) ,
- [Conservation]              char(1),
- [Global]                    char(1))
+Create schema Staging
+	Create table FactSpecies (
+	[ObservationID]             int identity primary key ,
+	[Site_Code]                 varchar(10) ,
+	[SpecieId]                  int  ,
+	[YearMonth]                 int  ,
+	[LOWERBOUND]                int  ,
+	[UPPERBOUND]                int ,
+	[CountingType]              char(10) ,
+	[Dataquality]               char(1) ,
+	[PopulationType]            char(1) ,
+	[AboundanceCategory]        char(1) ,
+	[Population]                char(1),
+	[Conservation]              char(1),
+	[Isolation]	                char(1),
+	[Global]                    char(1))
+
